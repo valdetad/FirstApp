0cf42b03acfba1176a7b12dca2a3126ec27c3f62
valdetademiri <valdeta.demiri@hotmail.com>
2023-02-15 22:08:13 +0100

    4 Commit

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Put here you widgets: (Initialize Widgets)
+    // To import any package or any widget package, you click "Alt + Enter
+    TextView textView;
+    Button button;
+    EditText editText;
+    ImageView imageView;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+    }}
