89661ec40b0191a6cce71b217f97f7a52c5a21c5
valdetademiri <valdeta.demiri@hotmail.com>
2023-02-17 13:36:25 +0100

    Final Commit

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.firstapp;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,5 +27,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText EmailAddress = (EditText) findViewById(R.id.EmailAddress);
+        EditText Password = (EditText) findViewById(R.id.Password);
 
+        Button button = (Button)findViewById(R.id.button) ;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(EmailAddress.getText().toString().equals("valdeta")&& Password.getText().toString().equals("valdeta")) {
+                  Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Sorry, your email or password was incorrect. Try Again!", Toast.LENGTH_SHORT).show();
+                }
+
+        });
     }}
